d2648b47ceebc3bcee9a32a84dbb4f892e240679
CHIA-YU TSAI <scorpio1106vt@gm.student.ncut.edu.tw>
2017-11-23 12:34:18 +0800

    單元二：產生 Form Request、設定 authorize()回傳 true、設定 rules()內回傳的驗證陣列並編輯 PostsController裡設定 type­hinting

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -14,7 +14,7 @@ class AdminPostsController extends Controller
         $data=['posts'=>$posts];
         return view('admin.posts.index',$data);
     }
-    public function store(Request $request)
+    public function store(PostRequest $request)
     {
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
@@ -31,7 +31,7 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
-    public function update(Request$request,$id)
+    public function update(PostRequest$request,$id)
     {
     $post=Post::find($id);
     $post->update($request->all());
